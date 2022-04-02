29efc9cd1247688e07a3d4334b444aa2f779ad72
ececi <huseyinkiceci@gmail.com>
2022-04-03 01:33:26 +0300

    KEY-6 - parallel bs config added for experimental issues

--- src/test/java/com/theherakles/keyacademy/utils/DriverUtil.java
+++ src/test/java/com/theherakles/keyacademy/utils/DriverUtil.java
@@ -4,14 +4,21 @@ import com.theherakles.keyacademy.enums.BrowserType;
 import com.theherakles.keyacademy.exception.AutomationException;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import java.io.FileReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Set;
 import lombok.NoArgsConstructor;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -21,6 +28,8 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 /**
@@ -139,5 +148,76 @@ public class DriverUtil {
     public static void closeDriver() {
         driverPool.get().quit();
         driverPool.remove();
+    }
+}
+
+class TestClass1 implements Runnable {
+    public void run() {
+        Hashtable<String, String> capsHashtable = new Hashtable<String, String>();
+        capsHashtable.put("device", "iPhone 12 Pro");
+        capsHashtable.put("real_mobile", "true");
+        capsHashtable.put("os_version", "14");
+        capsHashtable.put("build", "browserstack-build-1");
+        capsHashtable.put("name", "Thread 1");
+        mainTestClass r1 = new mainTestClass();
+        r1.executeTest(capsHashtable);
+    }
+}
+class TestClass2 implements Runnable {
+    public void run() {
+        Hashtable<String, String> capsHashtable = new Hashtable<String, String>();
+        capsHashtable.put("device", "Google Pixel 5");
+        capsHashtable.put("real_mobile", "true");
+        capsHashtable.put("os_version", "11.0");
+        capsHashtable.put("build", "browserstack-build-1");
+        capsHashtable.put("name", "Thread 2");
+        mainTestClass r2 = new mainTestClass();
+        r2.executeTest(capsHashtable);
+    }
+}
+
+class mainTestClass {
+    public static final String USERNAME = "hseyinkeeci_OFjRua";
+    public static final String AUTOMATE_KEY = "QepXX2yWg9cVpc27dGhy";
+    public static final String URL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
+    public static void main(String[] args) throws Exception {
+        Thread object1 = new Thread(new TestClass1());
+        object1.start();
+        Thread object2 = new Thread(new TestClass2());
+        object2.start();
+    }
+    public void executeTest(Hashtable<String, String> capsHashtable) {
+        String key;
+        DesiredCapabilities caps = new DesiredCapabilities();
+        // Iterate over the hashtable and set the capabilities
+        Set<String> keys = capsHashtable.keySet();
+        Iterator<String> itr = keys.iterator();
+        while (itr.hasNext()) {
+            key = itr.next();
+            caps.setCapability(key, capsHashtable.get(key));
+        }
+        WebDriver driver;
+        try {
+            driver = new RemoteWebDriver(new URL(URL), caps);
+            JavascriptExecutor jse = (JavascriptExecutor)driver;
+            // Searching for 'BrowserStack' on google.com
+            driver.get("https://www.google.com");
+            WebElement element = driver.findElement(By.name("q"));
+            element.sendKeys("BrowserStack");
+            element.submit();
+            // Setting the status of test as 'passed' or 'failed' based on the condition; if title of the web page contains 'BrowserStack'
+            WebDriverWait wait = new WebDriverWait(driver, 5);
+            try {
+                wait.until(ExpectedConditions.titleContains("BrowserStack"));
+                jse.executeScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\": \"passed\", \"reason\": \"Title matched!\"}}");
+            }
+            catch(Exception e) {
+                jse.executeScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\":\"failed\", \"reason\": \"Title not matched\"}}");
+            }
+            System.out.println(driver.getTitle());
+            driver.quit();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
     }
 }
